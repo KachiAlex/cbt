8cbc08ba1a59a3781ce7ed56c8b0543ab6b1be53
Kachi Alex <opd.livmind@gmail.com>
2025-09-01 11:42:15 +0100

    cleaned up frontend and backend temporary scripts

--- backend/check-db.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Checking Local Database...
-echo.
-echo This will show all collections and data in your local MongoDB...
-echo.
-node check-local-db.js
-echo.
-echo Database check completed!
-pause 
--- backend/run-migration.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting CBT Database Migration...
-echo.
-echo Make sure your local MongoDB is running!
-echo.
-node migrate-to-cloud.js
-echo.
-echo Migration completed!
-pause 
